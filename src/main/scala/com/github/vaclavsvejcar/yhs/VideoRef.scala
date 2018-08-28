ef98908f467c2a65cbd2b350f3a763e56a2f2a54
Vaclav Svejcar <v.svejcar@norcane.cz>
2018-08-28 15:31:52 +0200

    fixed missing implicits for HeaderEncoder[VideoRef]

--- src/main/scala/com/github/vaclavsvejcar/yhs/VideoRef.scala
+++ src/main/scala/com/github/vaclavsvejcar/yhs/VideoRef.scala
@@ -1,4 +1,12 @@
 package com.github.vaclavsvejcar.yhs
 
+import kantan.csv.HeaderEncoder
+
 final case class VideoRef(id: String, title: String, description: Option[String])
+
+object VideoRef {
+  implicit val headerEncoder: HeaderEncoder[VideoRef] =
+    HeaderEncoder.caseEncoder("id", "title", "description")(VideoRef.unapply)
+
+}
 
