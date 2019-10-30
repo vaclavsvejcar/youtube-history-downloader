2b8671ee4ddc712a8d1ce3751bd508085d91721b
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2019-10-30 15:48:25 +0100

    Fixed deprecated calls in tests

--- src/test/scala/com/github/vaclavsvejcar/yhd/tools/ParsersSpec.scala
+++ src/test/scala/com/github/vaclavsvejcar/yhd/tools/ParsersSpec.scala
@@ -13,7 +13,7 @@ object ParsersSpec extends TestSuite {
 
     import Parsers._
 
-    'testParseCookies - {
+    test("testParseCookies") {
       val testData1 =
         """
           |Cookie: NAME1=value1; NAME2=value2;
@@ -34,14 +34,14 @@ object ParsersSpec extends TestSuite {
       parseCookies(testData3) ==> Map.empty
     }
 
-    'testParseCToken - {
+    test("testParseCToken") {
       val expected = "theToken"
       val raw = s"/browse_ajax?action_continuation=1&direct_render=1&target_id=item-section-102652&continuation=$expected&foo=bar"
 
       parseCToken(raw) ==> expected
     }
 
-    'testParseSessionToken - {
+    test("testParseSessionToken") {
       val expected = "aaa"
       val raw =
         s"""
@@ -49,7 +49,7 @@ object ParsersSpec extends TestSuite {
            |      'XSRF_TOKEN': "$expected",
            |      'XSRF_FIELD_NAME': "session_token",
            |
-          |      'XSRF_REDIRECT_TOKEN': "bbb"  });
+           |      'XSRF_REDIRECT_TOKEN': "bbb"  });
            |yt.setConfig('ID_TOKEN', "ccc");window.ytcfg.set('SERVICE_WORKER_KILLSWITCH', false);  yt.setConfig('THUMB_DELAY_LOAD_BUFFER', 0);
            |if (window.ytcsi) {window.ytcsi.tick("jl", null, '');}
         """.stripMargin
@@ -57,8 +57,8 @@ object ParsersSpec extends TestSuite {
       parseSessionToken(raw) ==> expected
     }
 
-    'testParseVideoRefs - {
-      'testRemovedVideo - {
+    test("testParseVideoRefs") {
+      test("testRemovedVideo") {
         val source = Source.fromResource("removed-video-snippet.html").getLines().mkString("\n")
         val document = JsoupDocument(Jsoup.parse(source))
         val expected = VideoRef("aabbccdd", "The Video Title", None,
@@ -69,7 +69,7 @@ object ParsersSpec extends TestSuite {
         results.head ==> expected
       }
 
-      'testExistingVideo - {
+      test("testExistingVideo") {
         val source = Source.fromResource("existing-video-snippet.html").getLines().mkString("\n")
         val document = JsoupDocument(Jsoup.parse(source))
         val expected = VideoRef("aabbccdd", "The Video Title", Some("The Video Description"),
