ec8ce3b7fe323c7e9f17771d8d9cb29697bcff34
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2019-12-31 18:14:52 +0100

    Update Headroom template, refresh license headers

--- src/main/scala/com/github/vaclavsvejcar/yhd/package.scala
+++ src/main/scala/com/github/vaclavsvejcar/yhd/package.scala
@@ -1,6 +1,6 @@
 /*
  * YouTube History Downloader :: Download your full YouTube watch history
- * Copyright (c) 2019 Vaclav Svejcar
+ * Copyright (c) 2019-2020 Vaclav Svejcar
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
